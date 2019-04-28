574d5f1262660aee0b4cc8188a92252f411cab9e
devekiks <devekiks@users.noreply.github.com>
2019-04-28 18:26:37 +0300

    Dima1

--- test2/test2/Source.cpp
+++ test2/test2/Source.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 #include<vector>
+#include<string>
+
+using std::string;
+
+void myFunc(int x);
+	
 
 
 int main()
 {
+
+
 	for (int i = 0; i < 10; i++) std::cout << "Ada Gaming\n";
 	std::cout << "Hello GitHub\n";
-	int a, b;
+	int a=0;
+	string b;
 		std:: cout << "Hello GitHub\n";
 		std::cin >> a;
 		if (a == 1) {
@@ -15,11 +24,20 @@ int main()
 		else {
 			std::cout << "NePokemon\n";
 		}
-
+		myFunc(20);
 
 		std::cout << "Who is Pokemon?\n";
 		std::cin >> b;
-		std::cout >> b;
+		std::cout << b;
 	return 0;
 
+}
+
+void myFunc(int x)
+{
+	if (x != 0)
+	{
+		std::cout << "Hello\t" << x << std::endl;
+		myFunc(x - 5);
+	}
 }
