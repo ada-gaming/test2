b7ca36adf67eea162115ea3f1502724bd31b0bf5
DafBoy <6dafboy6@gmail.com>
2019-04-28 18:41:19 +0300

    удвоение вектора int

--- test2/test2/Source.cpp
+++ test2/test2/Source.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 
 
+std::vector<int> MpVector(std::vector<int> & x);
+
 int main()
 {
 	for (int i = 0; i < 10; i++) std::cout << "Ada Gaming\n";
@@ -19,7 +21,32 @@ int main()
 
 		std::cout << "Who is Pokemon?\n";
 		std::cin >> b;
-		std::cout >> b;
+		std::cout << b;
+		int x=0;
+		std::vector<int> old;
+		std::cout<<"Enter numbers\n\"q\" for Exit\n";
+		while (std::cin>>x)
+		{
+			old.push_back(x);
+		}
+		std::cout << "sqrt yours vector\n";
+		std::vector<int> n=MpVector(old);
+		for (int i = 0; i < n.size(); i++)
+		{
+			std::cout << n[i] << std::endl;
+		}
 	return 0;
 
+}
+
+std::vector<int> MpVector(std::vector<int> & x)
+{
+	std::vector<int>::iterator iter;
+	std::vector<int> y;
+	for (iter= x.begin(); iter != x.end(); iter++)
+	{
+		y.push_back(*iter  *  *iter);
+
+	}
+	return y;
 }
