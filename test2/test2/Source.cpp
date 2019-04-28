0a87d917541e18652bb9056492ac3243d62a8a64
DafBoy <6dafboy6@gmail.com>
2019-04-28 17:59:02 +0300

    new64

--- test2/test2/Source.cpp
+++ test2/test2/Source.cpp
@@ -6,6 +6,15 @@ int main()
 {
 	for (int i = 0; i < 10; i++) std::cout << "Ada Gaming\n";
 	std::cout << "Hello GitHub\n";
+	int a;
+		std:: cout << "Hello GitHub\n";
+		std::cin >> a;
+		if (a == 1) {
+			std::cout << "Pokemon\n";
+		}
+		else {
+			std::cout << "NePokemon\n";
+		}
 
 	return 0;
 
