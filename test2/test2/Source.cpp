5d0f8d0f2b3e3c2bf4b6f4a7929019db42959243
devekiks <devekiks@users.noreply.github.com>
2019-04-28 18:01:14 +0300

    new64

--- test2/test2/Source.cpp
+++ test2/test2/Source.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	for (int i = 0; i < 10; i++) std::cout << "Ada Gaming\n";
 	std::cout << "Hello GitHub\n";
-	int a;
+	int a, b;
 		std:: cout << "Hello GitHub\n";
 		std::cin >> a;
 		if (a == 1) {
@@ -15,7 +15,8 @@ int main()
 		else {
 			std::cout << "NePokemon\n";
 		}
-
+		std::cin >> b;
+		std::cout >> b;
 	return 0;
 
 }
